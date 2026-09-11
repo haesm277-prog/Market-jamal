ff0b98a0ed8de2eab58ecfdd7f78d815b5cd9f84
haesm277-prog <haesm277@gmail.com>
2026-09-12 02:13:21 +0300

    Add files via upload
    رفع المشروع الكامل

---
+++ android/app/src/main/java/com/superjamal/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.superjamal.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.superjamal.app',
+  appName: 'سوبر ماركت الجمال',
+  webDir: 'www'
+};
+
+export default config;
